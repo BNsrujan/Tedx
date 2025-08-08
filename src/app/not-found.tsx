e245712864471e43fe5b269657bcaf096c762e89
BNsrujan <srujanbn12@gmail.com>
2025-08-08 19:35:54 +0530

    removing the double cursur

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,4 +1,4 @@
-import Link from 'next/link'
+import Link from "next/link";
 
 export default function NotFound() {
   return (
@@ -7,20 +7,16 @@ export default function NotFound() {
         <h1 className="text-6xl font-bold text-red-600">404</h1>
         <h2 className="text-3xl font-light">Page Not Found</h2>
         <p className="text-gray-400 max-w-md mx-auto">
-          Oops! The page you're looking for seems to have vanished into thin air..
+          Oops! The page you're looking for seems to have vanished into thin
+          air..
         </p>
-        <Link 
+        <Link
           href="/"
           className="inline-block mt-8 px-6 py-3 bg-red-600 text-white  hover:bg-red-700 transition-colors duration-200"
         >
           Return Home
-
-
-
-
-
         </Link>
       </div>
     </div>
-  )
-} 
+  );
+}
