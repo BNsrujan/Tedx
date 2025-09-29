70910af2cd58fdd2f0bfb1926c639b556e660bbd
BNsrujan <srujanbn12@gmail.com>
2025-09-29 23:29:37 +0530

    justify center

--- src/app/organizers/page.tsx
+++ src/app/organizers/page.tsx
@@ -225,13 +225,13 @@ const OrganizerData: OrganizerData[] = [
 
 function Profile({ image, name, Tag }: ProfileProps) {
   return (
-    <div className="w-[150px]  text-center">
+    <div className="max-w-[150px]  text-center ">
       <Image
         src={image}
         alt={name}
         width={150}
         height={150}
-        className="grayscale"
+        className="grayscale rounded-xl object-cover mx-auto"
       />
       <p className="pt-2 text-white font-bold">{name}</p>
       <p className="pt-2 text-ted-red font-bold">{Tag}</p>
@@ -245,7 +245,7 @@ export default function Organizer() {
       <h3 className="text-5xl sm:text-6xl md:text-4xl lg:text-6xl uppercase text-white font-extrabold tracking-tight flex items-baseline justify-center gap-2 mb-10 mt-[30] md:mt-[100px]">
         Organizers
       </h3>
-      <div className="w-[900px] flex flex-col gap-16  ">
+      <div className="w-full max-w-6xl px-4 flex flex-col gap-16  ">
         {OrganizerData.map((organizerData, teamIndex) => (
           <div
             key={teamIndex}
@@ -254,7 +254,7 @@ export default function Organizer() {
             <h1 className=" w-full mx-0 text-4xl sm:text-5xl md:text-3xl lg:text-5xl Uppercase text-white font-extrobold  mb-6">
               {organizerData.Team}
             </h1>
-            <div className="flex gap-10">
+            <div className=" flex flex-wrap gap-10 justify-center items-center">
               {organizerData.Profile.map((profileData, profileIndex) => (
                 <Profile
                   key={profileIndex}
